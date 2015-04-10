9cd80f02523fb3934d4f2e48e0cb9939dc4b43f3
Neelsar <neelam.sar@gmail.com>
2015-04-10 18:41:30 +0530

    test

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,43 @@ $("#getData").click(function(){
 	  timeout: 300,
 	  context: $('body'),
 	  success: function(data){
-		console.log(data)
+		console.log(data);
+		//xml2json(data);
+		//$('footer').append(data)
 	  },
 	  error: function(xhr, type){
 		alert('Ajax error!')
 	  }
 	})
 });
+
+function xml2json(xml) {
+  try {
+    var obj = {};
+    if (xml.children.length > 0) {
+      for (var i = 0; i < xml.children.length; i++) {
+        var item = xml.children.item(i);
+        var nodeName = item.nodeName;
+
+        if (typeof (obj[nodeName]) == "undefined") {
+          obj[nodeName] = xml2json(item);
+        } else {
+          if (typeof (obj[nodeName].push) == "undefined") {
+            var old = obj[nodeName];
+
+            obj[nodeName] = [];
+            obj[nodeName].push(old);
+          }
+          obj[nodeName].push(xml2json(item));
+        }
+      }
+    } else {
+      obj = xml.textContent;
+    }
+    console.log(obj);
+  } catch (e) {
+      console.log(e.message);
+  }
+}
 
 
